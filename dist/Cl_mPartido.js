10eb8b3f606911c1f9c5a9cfc72ad0152d6614fa
MarieZu0 <mariazulay470@gmail.com>
2025-11-05 18:30:46 -0400

    251105-6:30: Inicial

---
+++ dist/Cl_mPartido.js
@@ -0,0 +1,46 @@
+export default class Cl_mPartido {
+    constructor() {
+        this.equipos = [];
+    }
+    agregarEquipo({ equipo, callback, }) {
+        // Validar nombre del equipo repetido
+        const nombreRepetido = this.equipos.find((e) => e.nombreEquipo.toLowerCase() === equipo.nombreEquipo.toLowerCase());
+        if (nombreRepetido) {
+            callback(`El equipo ${equipo.nombreEquipo} ya existe`);
+            return;
+        }
+        // Validar que el equipo no tenga menos de dos miembros
+        if (equipo.cantidadJugadores() < 2) {
+            callback(`El equipo ${equipo.nombreEquipo} debe tener al menos dos miembros.`);
+            return;
+        }
+        // Validar que el equipo no tenga más de cuatro miembros
+        if (equipo.cantidadJugadores() > 4) {
+            callback(`El equipo ${equipo.nombreEquipo} no puede tener más de cuatro miembros.`);
+            return;
+        }
+        // Validar cedula repetida
+        if (equipo.error()) {
+            callback(`El equipo ${equipo.nombreEquipo} tiene cédula repetida internamente.`);
+            return;
+        }
+        // Validar cedulas contra todos los equipos existentes
+        for (const e of this.equipos) {
+            if (e.existeCedula(equipo.cedula1) ||
+                e.existeCedula(equipo.cedula2) ||
+                e.existeCedula(equipo.cedula3) ||
+                e.existeCedula(equipo.cedula4)) {
+                callback(`El equipo ${equipo.nombreEquipo} tiene cédula repetida con el equipo ${e.nombreEquipo}.`);
+                return;
+            }
+        }
+        // Si todo está bien, agregar el equipo
+        this.equipos.push(equipo);
+        callback(false);
+    }
+    listar() {
+        let equipos = [];
+        this.equipos.forEach((e) => equipos.push(e.toJSON()));
+        return equipos;
+    }
+}
